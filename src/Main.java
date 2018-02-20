6a36bfb07bf8bea3772b0a8eefe3d59061f3fbd5
soumarRubaie <soumarruu@gmail.com>
2018-02-20 15:04:55 -0500

    commit3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,41 @@
+import java.util.Scanner;
 
 public class Main
 {
 
     public static void main(String[] args)
-    {
-        // TODO Auto-generated method stub
-        System.out.println("Soumar Bonjour tout le monde");
+    {       
+        
+        Scanner sc= new Scanner(System.in); 
+     
+        // LLC émetteur
+        TestThread A1=  new TestThread("A1");  
+        
+        // MAC émetteur 
+        TestThread A2 = new TestThread("A2");   
+        
+        // LLC récépteur
+        TestThread B1 = new TestThread("B1"); 
+        
+        //MAC récépteur 
+        TestThread B2 = new TestThread("B2"); 
+        
+        // Support de transmission 
+        TestThread C = new TestThread("C"); 
+            
+        // Lire la taille du tampon 
+        int tailleTampon = sc.nextInt(); 
+        
+        // Lire le délai de temporisation 
+        int timeOut = sc.nextInt(); 
+        
+        // Lire le nom du fichier a copier 
+        String nomFichier = sc.nextLine(); 
+        
+        // Lire l'emplacement de destination 
+        
+        String destination = sc.nextLine(); 
+        
     }
-
+    
 }
