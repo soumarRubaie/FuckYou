63090e6aac9860917b506f0fa6bfacd959b49fa8
Unknown <arnaud.ricaud.34@gmail.com>
2018-02-22 12:34:54 -0500

    Getters et Setters Trame

--- src/Trame.java
+++ src/Trame.java
@@ -1,9 +1,28 @@
 
 public class Trame
 {
-    int numeroTrame;   
+    private int numeroTrame;   
     private byte[] donnes;  
-    private int tailleTrame; 
+    private int tailleTrame;
+    
+	public int getNumeroTrame() {
+		return numeroTrame;
+	}
+	public void setNumeroTrame(int numeroTrame) {
+		this.numeroTrame = numeroTrame;
+	}
+	public byte[] getDonnes() {
+		return donnes;
+	}
+	public void setDonnes(byte[] donnes) {
+		this.donnes = donnes;
+	}
+	public int getTailleTrame() {
+		return tailleTrame;
+	}
+	public void setTailleTrame(int tailleTrame) {
+		this.tailleTrame = tailleTrame;
+	} 
     
     
 }
